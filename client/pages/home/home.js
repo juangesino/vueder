a5ab0fbbe596e9751eb7ac8b4b9fe5bf4b94c4e4
Juan Gesino <juangesino.pub@gmail.com>
2018-05-29 12:08:56 +0200

    Add extended forecast page

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -5,14 +5,22 @@ Tracker.autorun(function(){
       Session.set('currentWeather', result);
       return result;
     })
+    Meteor.call('getForecastWeather', geoLocation, function (err, result) {
+      Session.set('getForecastWeather', result);
+      return result;
+    })
   }
+});
+
+Template.home.onRendered(function() {
+   $.getScript("/assets/js/swipejs/swipe.js");
 });
 
 Template.home.onCreated(function () {
   Session.set('currentWeather', undefined);
 });
 
-Template.home.helpers({
+Template.currentWeather.helpers({
   getLocation: function () {
     if (Session.get('currentWeather') && Session.get('currentWeather').name) {
       let location = Session.get('currentWeather').name;
@@ -58,6 +66,54 @@ Template.home.helpers({
   }
 });
 
-Template.home.events({
-
-});
+Template.forecastWeather.helpers({
+  forecastList: function () {
+    if (Session.get('getForecastWeather') && Session.get('getForecastWeather').list) {
+      let list = Session.get('getForecastWeather').list.slice(0,10);
+      return list;
+    }
+  },
+  getLocation: function () {
+    if (Session.get('getForecastWeather') && Session.get('getForecastWeather').city) {
+      let location = Session.get('getForecastWeather').city.name;
+      return location;
+    } else {
+      return '';
+    }
+  },
+  getTime: function (data) {
+    if (data && data.dt_txt) {
+      let time = moment(data.dt_txt);
+      return time.format('hh:mm');;
+    }
+  },
+  getTemperature: function (data) {
+    if (data && data.main.temp) {
+      let temp = data.main.temp;
+      temp = temp - 273.15;
+      return Math.round(temp * 1)/1 + '°C';
+    } else {
+      return '';
+    }
+  },
+  getIcon: function (data) {
+    if (data && data.weather) {
+      let iconId = data.weather[0].id;
+      return 'wi wi-owm-' + iconId;
+    }
+  },
+  getMinTemperature: function (data) {
+    if (data && data.main.temp_min) {
+      let temp = data.main.temp_min;
+      temp = temp - 273.15;
+      return Math.round(temp) + '°C';
+    }
+  },
+  getMaxTemperature: function (data) {
+    if (data && data.main.temp_max) {
+      let temp = data.main.temp_max;
+      temp = temp - 273.15;
+      return Math.round(temp) + '°C';
+    }
+  },
+})
--- server/main.js
+++ server/main.js
@@ -16,4 +16,17 @@ Meteor.methods({
       return false;
     }
   },
+  getForecastWeather: function (args) {
+    this.unblock();
+    const lat = args.lat;
+    const lon = args.lng;
+    try {
+      const url = "http://api.openweathermap.org/data/2.5/forecast" + "?APPID=" + openWeatherKey + "&lat=" + lat + "&lon=" + lon;
+      const result = Meteor.http.get(url);
+      return result.data;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  },
 });
